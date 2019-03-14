fae73b7399a7a38311e9372fc5ec8763749b31f6
BP <cycumeister6969@gmail.com>
2019-03-14 23:51:59 +0100

    cmax

--- lab01/data.py
+++ lab01/data.py
@@ -5,8 +5,8 @@ def read_data_from_file(filename):
     list_of_tasks = []
     with open(filename) as file:
         i = 0
-        for line in file:
-            dt = list(map(int, re.findall(r'\d+', line)))
+        for f in file:
+            dt = list(map(int, re.findall(r'\d+', f)))
             if i==0:
                 numb_tasks = dt[0]
                 numb_machines = dt[1]
@@ -15,9 +15,12 @@ def read_data_from_file(filename):
             i += 1
     return list_of_tasks, numb_machines, numb_tasks
 
-a,b,c = read_data_from_file("test.txt")
-print(a[0].time[0])
-print(a[0].time[1])
+def fill_list_of_index(list_of_tasks):
+    list_of_index = []
+    for i in list_of_tasks:
+        list_of_index.append(i.nr)
+    return list_of_index
+
 
 
 
---
+++ lab01/na3.py
@@ -0,0 +1,33 @@
+from data import read_data_from_file
+from data import fill_list_of_index
+
+def cmax(numb_machines, list_of_index, list_of_tasks):
+    endtime = []
+    for i in range(numb_machines):
+        endtime.append(0)
+    for j in list_of_index:
+        endtime[0] += list_of_tasks[j].time[0]
+        for k in range(1, numb_machines):
+            if endtime[k] < endtime[k-1]:
+                endtime[k] = endtime[k-1]
+            endtime[k] += list_of_tasks[j].time[k]
+    return max(endtime)
+
+def permute(xs, low=0):
+    if low + 1 >= len(xs):
+        yield xs
+    else:
+        for p in permute(xs, low + 1):
+            yield p
+        for i in range(low + 1, len(xs)):
+            xs[low], xs[i] = xs[i], xs[low]
+            for p in permute(xs, low + 1):
+                yield p
+            xs[low], xs[i] = xs[i], xs[low]
+
+
+list_of_tasks, numb_machines, numb_tasks = read_data_from_file("test.txt");
+ind = fill_list_of_index(list_of_tasks)
+makespan = cmax(numb_machines, ind, list_of_tasks)
+print(makespan)
+
