c5165222b0b28a35ee8ce66cf2710aa53390fc5f
Michal <michalxbocian@gmail.com>
2019-03-25 01:30:51 +0100

    test prepared for more files

--- lab01/tests/CMAX/test_Cmax.py
+++ lab01/tests/CMAX/test_Cmax.py
@@ -1,11 +1,16 @@
 from Agregator_dir.Agregator_class import Agregator
 
 
-correct_values = 31
+correct_values = [31, ]
+files = ['test.txt',
+]
 
-def test_should_pass_if_Cmax_calculated_correctly(correct_values):
-	agregator = Agregator("test.txt")
-	assert agregator.Cmax_test() == correct_values
-	print("PASSED - test_should_pass_if_Cmax_calculated_correctly")
 
-test_should_pass_if_Cmax_calculated_correctly(correct_values)
+def test_should_pass_if_Cmax_calculated_correctly(correct_values, files):
+	for file in files:
+		for cmax in correct_values:
+			agregator = Agregator(file)
+			assert agregator.Cmax_test() == cmax
+			print("PASSED - test_should_pass_if_Cmax_calculated_correctly")
+
+test_should_pass_if_Cmax_calculated_correctly(correct_values, files)
